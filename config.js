f1d109e557a74360e61586e5470a591972ec867a
Muhammad Hassam <thehassamriaz@gmail.com>
2022-10-10 18:48:36 +0500

    New file added

---
+++ config.js
@@ -0,0 +1,12 @@
+class Config {
+
+    test() {
+        let options = {
+            "class_1": 1,
+            "class_2": 2,
+            "class_3": 3,
+            "class_4": 4,
+        };
+    }
+
+}
